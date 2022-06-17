902e4a69c9cfc5142b662dc2e4551f00fe5f6cf2
Joe Keen <joe@keen.id.au>
2022-06-17 19:13:25 +1000

    Add initial files from Spring Initializr.

---
+++ src/main/java/au/id/keen/pfm/ProcessedFutureMovementApplication.java
@@ -0,0 +1,13 @@
+package au.id.keen.pfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessedFutureMovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessedFutureMovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/id/keen/pfm/ProcessedFutureMovementApplicationTests.java
@@ -0,0 +1,13 @@
+package au.id.keen.pfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessedFutureMovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
